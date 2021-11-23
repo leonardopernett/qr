6c5df20f537381ca9eea64300ea6a516d9ec839b
jonathan arroyave morales <titodelta321@hotmail.com>
2021-11-23 16:35:02 -0500

    primer commit funcional
    primer commit funcional

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,15 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Konecta | Quejas - Reclamos</title>
-    <link rel="shortcut icon" href="{{ asset('k1.png') }}" type="image/x-icon">
+    <!-- <link rel="shortcut icon" href="{{ asset('k1.png') }}" type="image/x-icon"> -->
+    <link rel="shortcut icon" href="../public/k1.png" type="image/x-icon">
     {{-- bootstrap 5 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
    
     {{-- style --}}
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('/css/main.css') }}"> -->
+    <link rel="stylesheet" href="../public/css/main.css">
   
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     @yield('styles')
 </head>
 <body>
@@ -37,8 +39,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>  
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" ></script>
-    <script src="{{ asset('js/vendor/ckeditor/ckeditor.js')}}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="../public/js/vendor/ckeditor/ckeditor.js"></script>
+    <!-- <script src="{{ asset('js/vendor/ckeditor/ckeditor.js')}}"></script> -->
+    <script src="../public/js/main.js"></script>
+    <!-- <script src="{{ asset('js/main.js') }}"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src=" //cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"> </script>
 
