97c3d513f638eaae84285461c8b68463c834c5d2
leonardo <julian.vargas@grupokonecta.com>
2021-12-07 08:31:03 -0600

    add integrity2

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -35,7 +35,6 @@
 
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>   --}}
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
